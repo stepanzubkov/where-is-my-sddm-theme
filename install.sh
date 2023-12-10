d8ef32b391d499c1e0897108a9d92c55536c54bd
Stepan Zubkov <stepanzubkov@florgon.com>
2023-12-10 11:53:52 +0300

    feat(scripts): Add install.sh script.

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp -vr where_is_my_sddm_theme/ /usr/share/sddm/themes/
+echo "Successfully installed!"
+
+if [[ $1 == "current" ]]; then
+    sed -i 's/Current=.*/Current=where_is_my_sddm_theme/' /etc/sddm.conf.d/kde_settings.conf
+fi
